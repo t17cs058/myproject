802d59395cbff563e057493da122c64044f40b25
t17cs015 <t17cs015@dw11.kki.yamanashi.ac.jp>
2019-11-20 16:01:44 +0900

    viewにshowクラスを実装

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -5,4 +5,8 @@ appname = "shoppinglist"
 
 urlpatterns = [
     path("list/", ItemList.as_view(), name="list"),  # @UndefinedVariable
+    # path('add', ItemAddView.as_view(),name='add'),
+    # path('show', ItemShowView.as_view(),name='show'),
+    # path('edit', ItemEditView.as_view(),name='edit'),
+    # path('delete', ItemDeleteView.as_view(),name='delete'),
     ]
