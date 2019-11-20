396a877429883231ac49293de4ae4673683ae279
kohei nishimuta <einosturmi142@gmail.com>
2019-11-20 16:13:41 +0900

    [add] 削除されるよう post メソッドを実装

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -27,6 +27,12 @@ class ItemList(ListView):
 class ItemDeleteView(TemplateView):
     model = Item
     template_name = "shoppinglist/item_delete.html"
+    
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get("item_id")
+        item = get_object_or_404(Item, pk=item_id)
+        item.delete()
+        return HttpResponseRedirect(reverse("list"))
 
     def get_context_data(self, **kwarg):
         context = super().get_context_data(**kwarg)
