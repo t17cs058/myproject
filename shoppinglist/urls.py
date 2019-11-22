b16d78c066cbb9ba6ebbe962a421677362584104
t17cs027 <t17cs027@dw09.kki.yamanashi.ac.jp>
2019-11-22 14:28:13 +0900

    編集1

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path # @UnresolvedImport
-from .views import ItemList
+from .views import ItemList,ItemEditView
 
 appname = "shoppinglist"
 
 urlpatterns = [
-    path("list/", ItemList.as_view(), name="list"),  # @UndefinedVariable
+     path("list/", ItemList.as_view(), name="list"),
+     path('edit', ItemEditView.as_view(),name='edit'),
+     # @UndefinedVariable
     ]
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -4,8 +4,10 @@ from .models import Item
 from django.http import HttpResponseRedirect # @UnresolvedImport
 from django.urls import reverse # @UnresolvedImport
 from django.shortcuts import get_object_or_404 # @UnresolvedImport
-from .forms import ItemBuy
+from .forms import ItemBuy, ItemIdForm, ItemForm
 from lib2to3.fixes.fix_input import context
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 
 class ItemList(ListView):
     model = Item
@@ -22,4 +24,28 @@ class ItemList(ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = ItemBuy()
         return context
-    
+
+
+class ItemEditView(TemplateView):
+    model = Item
+    template_name = 'shoppinglist/item_edit.html'
+    success_url = 'list/'
+
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get('item_id')
+        name = self.request.POST.get('name')
+        count = self.request.POST.get('count')
+        buy_date = self.request.POST.get('buy_date')
+
+        item = get_object_or_404(Item, pk=item_id)
+        item.name = name
+        item.count = count
+        item.buy_date = buy_date
+        item.save()
+        return HttpResponseRedirect(reverse('list'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        context['form'] = ItemForm()
+        return context
