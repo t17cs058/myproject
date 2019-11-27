f8190a4d713abaaaebbf2acb8bf1e6fa5ab5cce4
t17cs015 <t17cs015@dw11.kki.yamanashi.ac.jp>
2019-11-27 15:44:09 +0900

    master の修正

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -3,7 +3,7 @@ from .models import Item
 from django.http import HttpResponseRedirect # @UnresolvedImport
 from django.urls import reverse # @UnresolvedImport
 from django.shortcuts import get_object_or_404 # @UnresolvedImport
-from .forms import ItemBuy, ItemIdForm # @UnresolvedImport
+from .forms import ItemBuy, ItemIdForm , ItemForm# @UnresolvedImport
 from django.views.generic.base import TemplateView # @UnresolvedImport
 from lib2to3.fixes.fix_input import context
 from django.views.generic.base import TemplateView
@@ -26,27 +26,27 @@ class ItemList(ListView):
         return context
 
 class ItemAddView(CreateView):
-     model = Item
-     fields = ('name', 'item_url', 'count', 'buy_date', 'shop')
-     template_name = 'shoppinglist/item_add.html'
-     success_url = 'list/'
+    model = Item
+    fields = ('name', 'item_url', 'count', 'buy_date', 'shop')
+    template_name = 'shoppinglist/item_add.html'
+    success_url = 'list/'
     
 class ItemShowView(TemplateView):
-     model = Item
-     template_name = 'shoppinglist/item_show.html'
+    model = Item
+    template_name = 'shoppinglist/item_show.html'
 
-     def post(self, request, *args, **kwargs):
-         item_id = self.request.POST.get('item_id')
-         item = Item.objects.get(pk=item_id)
-         context = super().get_context_data(**kwargs)
-         context['form_id'] = ItemIdForm()
-         context['item'] = item
-         return self.render_to_response(context)
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get('item_id')
+        item = Item.objects.get(pk=item_id)
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        context['item'] = item
+        return self.render_to_response(context)
 
-     def get_context_data(self, **kwargs):
-         context = super().get_context_data(**kwargs)
-         context['form_id'] = ItemIdForm()
-         return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        return context
 
 
 class ItemEditView(TemplateView):
@@ -73,7 +73,19 @@ class ItemEditView(TemplateView):
         context['form'] = ItemForm()
         return context
 
-
 class ItemDeleteView(TemplateView):
+    model = Item
+    template_name = "shoppinglist/item_delete.html"
+
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get("item_id")
+        item = get_object_or_404(Item, pk=item_id)
+        item.delete()
+        return HttpResponseRedirect(reverse("list"))
+
+    def get_context_data(self, **kwarg):
+        context = super().get_context_data(**kwarg)
+        context["form"] = ItemIdForm()
+        return context
 
 
