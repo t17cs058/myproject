0650180d9eb83c40da5ac62d27dff8729ef47345
t17cs015 <t17cs015@dw11.kki.yamanashi.ac.jp>
2019-11-27 17:54:58 +0900

    りダイレクトの修正

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -6,9 +6,9 @@ appname = "shoppinglist"
 urlpatterns = [
 
     path("list/", ItemList.as_view(), name="list"),  # @UndefinedVariable
-    path('add/', ItemAddView.as_view(),name='add'),  # @UndefinedVariable
+    path('add', ItemAddView.as_view(),name='add'),  # @UndefinedVariable
     path('edit', ItemEditView.as_view(),name='edit'),# @UndefinedVariable
-    path('show/', ItemShowView.as_view(),name='show'), # @UndefinedVariable
-    path("delete/", ItemDeleteView.as_view(), name="delete"),  # @UndefinedVariable
+    path('show', ItemShowView.as_view(),name='show'), # @UndefinedVariable
+    path("delete", ItemDeleteView.as_view(), name="delete"),  # @UndefinedVariable
 
     ]
