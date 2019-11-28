fa0f90f199278971713946dc1558542efc9a5f47
t17cs012 <t17cs012@yamanashi.ac.jp>
2019-11-28 18:27:59 +0900

    [add]add機能の追加

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -9,10 +9,10 @@ appname = "shoppinglist"
 urlpatterns = [
 
     path("list/", ItemList.as_view(), name="list"),  # @UndefinedVariable
-    path('add/', ItemAddView.as_view(),name='add'),  # @UndefinedVariable
+    path('add', ItemAddView.as_view(),name='add'),  # @UndefinedVariable
     path('edit', ItemEditView.as_view(),name='edit'),# @UndefinedVariable
-    path('show/', ItemShowView.as_view(),name='show'), # @UndefinedVariable
-    path("delete/", ItemDeleteView.as_view(), name="delete"),  # @UndefinedVariable
+    path('show', ItemShowView.as_view(),name='show'), # @UndefinedVariable
+    path("delete", ItemDeleteView.as_view(), name="delete"),  # @UndefinedVariable
 
 
 
