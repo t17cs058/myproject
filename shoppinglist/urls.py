c54adaaac5a23cd1bd45af59278541e8d87232c9
t17cs012 <t17cs012@yamanashi.ac.jp>
2019-12-06 16:57:52 +0900

    [add]buyビューを追加して購入の切り替えをシンプルにした

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path # @UnresolvedImport
-from .views import ItemList ,ItemAddView,ItemEditView, ItemShowView,ItemDeleteView # @UnresolvedImport
+from .views import ItemList ,ItemAddView,ItemEditView, ItemShowView,ItemDeleteView,ItemBuyView# @UnresolvedImport
 
 appname = "shoppinglist"
 
@@ -10,7 +10,10 @@ urlpatterns = [
     path('edit', ItemEditView.as_view(),name='edit'),# @UndefinedVariable
     path('show', ItemShowView.as_view(),name='show'), # @UndefinedVariable
     path("delete", ItemDeleteView.as_view(), name="delete"),  # @UndefinedVariable
+    path("buy", ItemBuyView.as_view(), name="buy"),  # @UndefinedVariable
     path('delete/<int:item_id>', ItemDeleteView.as_view(), name='delete'),
     path('edit/<int:item_id>', ItemEditView.as_view(), name='edit'),
+    path('list/<int:item_id>', ItemList.as_view(), name='list'),
+    path("buy/<int:item_id>", ItemBuyView.as_view(), name="buy"),
 
     ]
